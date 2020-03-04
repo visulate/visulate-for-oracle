5b9654fff0a23d24e4196ad2a818866cd4bd0de1
Peter Goldthorp <peter@goldthorp.com>
2020-03-04 13:07:19 -0500

    Create http-server.js

---
+++ api-server/config/http-server.js
@@ -0,0 +1,8 @@
+/**
+ * Return environment variable or default value 
+ */
+module.exports = {
+  port: process.env.HTTP_PORT || 3000 ,
+  corsOriginWhitelist: process.env.CORS_ORIGIN_WHITELIST ||['http://localhost:4200', 'http://localhost'],
+  logFileLocation: process.env.PWD + '/logs'
+};
