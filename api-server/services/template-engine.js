597d2fb73235415e91847f495b79b9e59f77c095
pgoldtho <peter@goldthorp.com>
2020-10-15 11:14:49 -0400

    pl/sql call template

--- api-server/services/template-engine.js
+++ api-server/services/template-engine.js
@@ -17,20 +17,59 @@ const handlebars = require('handlebars');
 const fs = require('fs');
 const path = require('path');
 
-handlebars.registerHelper('ifEquals', function(arg1, arg2, options) {
+// Return true if arg1 == arg2 else false
+handlebars.registerHelper('ifEquals', function (arg1, arg2, options) {
   return (arg1 == arg2) ? options.fn(this) : options.inverse(this);
 });
 
-handlebars.registerHelper('unlessEquals', function(arg1, arg2, options) {
-    return (arg1 != arg2) ? options.fn(this) : options.inverse(this);
-  });
+// Return true if arg1 != arg2 else false
+handlebars.registerHelper('unlessEquals', function (arg1, arg2, options) {
+  return (arg1 != arg2) ? options.fn(this) : options.inverse(this);
+});
 
+// Return true if arg appears in csv
+handlebars.registerHelper('ifAppearsIn', function (arg, csv, options){
+  const list = csv.split(',');
+  return (list.includes(arg)) ? options.fn(this) : options.inverse(this);
+});
+
+// Return true if arg does not in csv
+handlebars.registerHelper('unlessAppearsIn', function (arg, csv, options){
+  const list = csv.split(',');
+  return (list.includes(arg)) ? options.inverse(this): options.fn(this);
+});
+
+// Assign varName = varValue
 handlebars.registerHelper('assign', function (varName, varValue, options) {
   if (!options.data.root) {
-      options.data.root = {};
+    options.data.root = {};
   }
   options.data.root[varName] = varValue;
 });
+
+// Append a value to an assigned variable
+handlebars.registerHelper('append', function (varName, varValue, options) {
+  if (!options.data.root) {
+    options.data.root = {};
+  }
+  const appendedValue = options.data.root[varName] + varValue;
+  options.data.root[varName] = appendedValue;
+});
+
+// Return true if value assigned to levelName is different to newLevel else false
+handlebars.registerHelper('ifLevelChanged', function (levelName, newLevel, options) {
+  if (!options.data.root) {
+    options.data.root = {};
+  }
+  const oldLevel = options.data.root[levelName];
+  if (oldLevel == newLevel) {
+    return options.inverse(this);
+  } else {
+    options.data.root[levelName] = newLevel;
+    return options.fn(this)
+  }
+});
+
 
 /**
  * Mutate object by stripping space characters from keys
@@ -39,85 +78,85 @@ handlebars.registerHelper('assign', function (varName, varValue, options) {
  * @param {} object - the object to mutate
  */
 function removeSpacesInKeys(object) {
-    Object.keys(object).forEach(function (key) {
-        const newKey = key.replace(/\s+/g, '');
-        if (object[key] && typeof object[key] === 'object') {
-          removeSpacesInKeys(object[key]);
+  Object.keys(object).forEach(function (key) {
+    const newKey = key.replace(/\s+/g, '');
+    if (object[key] && typeof object[key] === 'object') {
+      removeSpacesInKeys(object[key]);
+    }
+    if (key !== newKey) {
+      object[newKey] = object[key];
+      delete object[key];
+    }
+  });
+}
+
+/**
+ * Refactor the showObject's return value for use in mustache template
+ * @param {*} resultObject - objectDetails value from showObject
+ */
+function refactorResultObject(resultObject) {
+  let refactoredObject = resultObject.map(obj => {
+    let rObj = {};
+    rObj[obj.title] = obj.rows;
+    return rObj;
+  });
+  return refactoredObject
+}
+
+/**
+ * Apply a handlebars template to the resultSet.
+ *
+ * applyTemplate can be called from the showObject function to format an object
+ * report or listObjects to format a list of database objects. The resultType
+ * parameter is used to indicate the source/type of data. Object report results are
+ * refactored to simplify templating. Report results are passed in using the resultSet
+ * parameter.
+ *
+ * The template is passed as a query parameter and references a handlebars or mustache
+ * template in the {api-root}/config/templates directory. applyTemplate returns a promise
+ * which resolves when the template has been applied or rejects if passed an invalid
+ * template name. Passing a template value of 'none' resolves by returning the refactored
+ * resultSet that gets passed to the template for formatting. This can be used during
+ * template development.
+ *
+ * @param {*} resultType - 'object' or 'list'
+ * @param {*} resultSet - data to pass to the template
+ * @param {*} req - the http request object
+ */
+function applyTemplate(resultType, resultSet, req) {
+  return new Promise(function (resolve, reject) {
+    const template = req.query.template;
+    let resultObject = new Object();
+    resultObject.runDate = new Date().toDateString();
+    resultObject.protocol = req.protocol;
+    resultObject.host = req.headers.host;
+    resultObject.url = req.url;
+    resultObject.params = req.params;
+    resultObject.queryParams = req.query;
+    resultType === 'object' ?
+      resultObject.results = refactorResultObject(resultSet) : resultObject.results = resultSet;
+    removeSpacesInKeys(resultObject);
+
+    if (template === 'none') {
+      resolve(resultObject);
+    } else {
+      const templateFile = path.normalize(`${__dirname}/../config/templates/${template}`);
+      fs.readFile(templateFile, 'utf8', (err, tpl) => {
+        if (err) {
+          reject(`Template ${template} not found`);
+          return;
         }
-        if (key !== newKey) {
-            object[newKey] = object[key];
-            delete object[key];
+        try {
+          const compiledTemplate = handlebars.compile(tpl);
+          const transformed = compiledTemplate(resultObject);
+          resolve(transformed)
+        } catch (error) {
+          reject(`Template error: "${error}"`);
+          return;
         }
-    });
-  }
-
-  /**
-   * Refactor the showObject's return value for use in mustache template
-   * @param {*} resultObject - objectDetails value from showObject
-   */
-  function refactorResultObject(resultObject) {
-    let refactoredObject = resultObject.map(obj => {
-      let rObj = {};
-      rObj[obj.title] = obj.rows;
-      return rObj;
-    } );
-    return refactoredObject
-  }
-
-  /**
-   * Apply a handlebars template to the resultSet.
-   *
-   * applyTemplate can be called from the showObject function to format an object
-   * report or listObjects to format a list of database objects. The resultType
-   * parameter is used to indicate the source/type of data. Object report results are
-   * refactored to simplify templating. Report results are passed in using the resultSet
-   * parameter.
-   *
-   * The template is passed as a query parameter and references a handlebars or mustache
-   * template in the {api-root}/config/templates directory. applyTemplate returns a promise
-   * which resolves when the template has been applied or rejects if passed an invalid
-   * template name. Passing a template value of 'none' resolves by returning the refactored
-   * resultSet that gets passed to the template for formatting. This can be used during
-   * template development.
-   *
-   * @param {*} resultType - 'object' or 'list'
-   * @param {*} resultSet - data to pass to the template
-   * @param {*} req - the http request object
-   */
-  function applyTemplate(resultType, resultSet, req) {
-    return new Promise (function(resolve, reject){
-      const template = req.query.template;
-      let resultObject = new Object();
-      resultObject.runDate = new Date().toDateString();
-      resultObject.protocol = req.protocol;
-      resultObject.host = req.headers.host;
-      resultObject.url = req.url;
-      resultObject.params = req.params;
-      resultObject.queryParams = req.query;
-      resultType === 'object' ?
-        resultObject.results = refactorResultObject(resultSet): resultObject.results = resultSet;
-      removeSpacesInKeys(resultObject);
-
-      if (template === 'none') {
-        resolve(resultObject);
-      } else {
-        const templateFile = path.normalize(`${__dirname}/../config/templates/${template}`);
-        fs.readFile(templateFile, 'utf8', (err, tpl) => {
-          if (err) {
-            reject(`Template ${template} not found`);
-            return;
-          }
-          try {
-              const compiledTemplate = handlebars.compile(tpl);
-              const transformed = compiledTemplate(resultObject);
-              resolve(transformed)
-          } catch (error) {
-              reject (`Template error: "${error}"`);
-              return;
-          }
-        });
-      }
-    });
-  }
+      });
+    }
+  });
+}
 
 module.exports.applyTemplate = applyTemplate;
