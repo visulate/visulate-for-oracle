0ad81d9a8185788195ad8ec532ecf00c7539a977
pgoldtho <peter@goldthorp.com>
2024-01-14 14:44:38 -0500

    Integrate codegen with UI

---
+++ ui/src/environments/hbs-templates.ts
@@ -0,0 +1,25 @@
+// Map containing the Handlebars templates used by the application.
+export const hbsTemplates = new Map();
+hbsTemplates.set("TABLE", [
+  {title: "CRUD Package", template: "plsql_api_gen.hbs"},
+  {title: "Checksum View", template: "checksum_view.hbs"},
+  {title: "CSV Dump Script", template: "csv_gen.hbs"},
+  {title: "SQL*Loader Control File", template: "gen_sql_loader_control_file.hbs"},
+  {title: "Google BigQuery DDL", template: "bqddl.hbs"}
+]);
+
+hbsTemplates.set("VIEW", [
+  {title: "CSV Dump Script", template: "csv_gen.hbs"},
+]);
+
+hbsTemplates.set("MATERIALIZED VIEW", [
+  {title: "CSV Dump Script", template: "csv_gen.hbs"},
+]);
+
+hbsTemplates.set("PACKAGE", [
+  {title: "Procedure and function calls", template: "plsql_package_call.hbs"},
+]);
+
+hbsTemplates.set("PACKAGE BODY", [
+  {title: "Node JS SQL Statements", template: "plsql2nodejs.hbs"},
+]);
