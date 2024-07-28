c9087388ea29d3d1d9482fe7fed81c8f83bb0593
pgoldtho <peter@goldthorp.com>
2024-07-28 16:58:19 -0400

    remove commented code

--- query-engine/setup.py
+++ query-engine/setup.py
@@ -1,27 +1,9 @@
 from setuptools import find_packages, setup
-# setup(
-#     name='sql2csv',
-#     version='2.0.0',
-#     packages=find_packages(),
-#     include_package_data=True,
-#     zip_safe=False,
-#     install_requires=[
-#         'flask',
-#         'flask_cors',
-#         'simplejson',
-#         'sqlparse'
-#     ],
-# )
 setup(
     name="sql2csv",
     version="2.0.0",
-    # author="Your Name",
-    # author_email="your.email@example.com",
-    # description="Description of your package",
-    # long_description=open('README.md').read(),
-    # long_description_content_type='text/markdown',
     url="https://github.com/yourusername/sql2csv",
-    packages=find_packages(include=['sql2csv', 'sql2csv.*']),  # Ensure all subpackages are included
+    packages=find_packages(include=['sql2csv', 'sql2csv.*']),
     install_requires=[
         "flask",
         "flask_cors",
